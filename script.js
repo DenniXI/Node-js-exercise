afc15794c264898edf824dbbb8b4d86efc33c5ff
Dennis <emanuele.glorioso@gmail.com>
2023-11-24 16:48:23 +0100

    exercise-9

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const fs = require('node:fs');
+const players = ["Joe", "Caroline", "Sabrina"];
 
-fs.writeFile("file-1.txt", "Hello everyone!",(error, data) => {
-    if (error){
-        console.error(error)
-        return;
-    }
-    console.log(data)
-})
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+        const win = Boolean(Math.round(Math.random()));
+
+        process.nextTick(() => {
+            if (win) {
+                resolve(`${player} won a prize in the draw!`);
+            } else {
+                reject(new Error(`${player} lost the draw.`));
+            }
+        });
+
+        players.forEach(el => {
+            luckyDraw(el)
+                .then((value) => console.log(value))
+                .catch((error) => console.error(error))
+        })
+    });
+}
