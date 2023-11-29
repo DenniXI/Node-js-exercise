c8476e8a650c68b9399657ba753165796189b790
Dennis <emanuele.glorioso@gmail.com>
2023-11-29 22:20:33 +0100

    exercise-14

--- nodejs-esercizi/src/server.ts
+++ nodejs-esercizi/src/server.ts
@@ -1,63 +1,28 @@
 const express = require('express')
 const morgan =  require('morgan')
 import 'express-async-errors'
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+} from "./planets"
+
 const port = 3000
 const app = express()
 app.use(morgan("dev"))
 
-type Planet = {
-    id: number,
-    name: string,
-  };
 
-  type Planets = Planet[];
+app.get('/api/planets', getAll)
 
-  let planets: Planets = [
-    {
-      id: 1,
-      name: "Earth",
-    },
-    {
-      id: 2,
-      name: "Mars",
-    },
-  ];
+app.get("/api/planets/:id", getOneById);
 
+app.post('/api/planets', create)
 
-app.get('/api/planets', (req, res) => {
-    res.status(200).json(planets)
-})
+app.put('/api/planets/:id', updateById)
 
-app.get("/api/planets/:id", (req, res) => {
-  const id = JSON.parse(req.params.id);
-  const findUser = planets.filter((planet) => planet.id === id);
-
-  if (findUser.length > 0) {
-    res.status(200).json(findUser);
-  } else {
-    res.status(500).send("Errore interno");
-  }
-});
-
-app.post('/api/planets', (req, res) => {
-    const { id, name } = req.body
-    const newPlanet = { id, name}
-    planets = [...planets, newPlanet]
-    res.status(201).json({msg:'Planet created.'})
-})
-
-app.put('/api/planets/:id', (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-  planets = planets.map((p) => p.id === Number(id) ? ({...p, name}) : p)
-  res.status(200).json({msg:'Planet updated.'})
-})
-
-app.delete('/api/planets/:id', (req, res) => {
-  const { id } = req.params
-  planets = planets.filter((p)=> p.id !== Number(id))
-  res.status(200).json({msg: 'Planet deleted.'})
-})
+app.delete('/api/planets/:id', deleteById)
 
 app.listen(port, ()=>{
     console.log(`Example app listening on https://localhost:${port}`)
