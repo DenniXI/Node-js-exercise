3f70835e8fd294c159f86c11bf737aff96b4a4ac
Dennis <emanuele.glorioso@gmail.com>
2023-12-13 11:35:40 +0100

    exercise-17

---
+++ nodejs-esercizi/src/authorize.ts
@@ -0,0 +1,16 @@
+import passport from 'passport'
+import  { Request, Response, NextFunction }  from 'express';
+
+
+const authorize = async (req: Request, res: Response, next:NextFunction) => {
+    passport.authenticate("jwt",{ session: false }, (err: any, user:any)=>{
+        if(!user || err){
+            res.status(401).json({msg: "Unauthorized"});
+        }else{
+           req.user = user;
+           next();
+        }
+    })(req, res, next)
+}
+
+export default {authorize}
--- nodejs-esercizi/src/server.ts
+++ nodejs-esercizi/src/server.ts
@@ -3,6 +3,8 @@ import morgan from 'morgan';
 import 'express-async-errors';
 import { getAll, getOneById, create, updateById, deleteById, createImage } from "./controllers/planets";
 import multer from "multer"
+import { logIn, signUp } from './controllers/users'
+import "./passport.js"
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -32,6 +34,9 @@ app.put('/planets/:id', updateById);
 app.delete('/planets/:id', deleteById);
 
 app.post('planets/:id/image', upload.single("image"), createImage);
+
+app.post('/users/login', logIn);
+app.post('/users/signup', signUp);
 
 app.listen(port, ()=>{
     console.log(`Example app listening on http://localhost:${port}`)
